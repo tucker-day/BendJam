6e65865f10c01da92331dd92a45378bfb894ee69
Clare <72578735+clarechan1738@users.noreply.github.com>
2025-02-26 12:00:37 -0500

    Grading System Semi-Added
    - Grade Letters & Colors Change With Score (0-100; S, A, B, C, D, F)
    - Score Management test scene added with functionality & score manager script

---
+++ Assets/Scripts/GradeManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GradeManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private TMP_Text gradeTxt;
+
+    [SerializeField]
+    private int finalGrade = 100;
+
+    private void Start()
+    {
+        gradeTxt.text = string.Empty;
+    }
+
+    private void Update()
+    {
+        switch(CalcGrade(finalGrade))
+        {
+            case 0:
+                gradeTxt.text = "S";
+                //Text Color Is Dark Green For Best
+                gradeTxt.color = new Color(0.569f, 0f, 1f, 1.0f);
+                break;
+            case 1:
+                gradeTxt.text = "A";
+                //Text Color Is Light Green For Pass
+                gradeTxt.color = new Color(0f, 0.89f, 0.21f, 1.0f);
+                break;
+            case 2:
+                gradeTxt.text = "B";
+                //Text Color Is Yellow For Meh
+                gradeTxt.color = new Color(0.91f, 1f, 0f, 1.0f);
+                break;
+            case 3:
+                gradeTxt.text = "C";
+                //Text Color Is Orange For Not Very Good Lil Bro
+                gradeTxt.color = new Color(1f, 0.5f, 0f, 1.0f);
+                break;
+            case 4:
+                gradeTxt.text = "D";
+                //Text Color Is Dark Orange For Brink Of Fail
+                gradeTxt.color = new Color(1f, 0.25f, 0f, 1.0f);
+                break;
+            case 5:
+                gradeTxt.text = "F";
+                //Text Color Is Red For Fail
+                gradeTxt.color = new Color(0.9f, 0f, 0.03f, 1.0f);
+                break;
+        }
+
+    }
+
+    private int CalcGrade(int grade)
+    {
+        if(finalGrade >= 95 && finalGrade <= 100)
+        {
+            return 0;
+        }
+        else if(finalGrade >= 80 && finalGrade <= 94)
+        {
+            return 1;
+        }
+        else if(finalGrade >= 65 && finalGrade <= 79)
+        {
+            return 2;
+        }
+        else if(finalGrade >= 50 && finalGrade <= 64)
+        {
+            return 3;
+        }
+        else if(finalGrade >= 35 && finalGrade <= 49)
+        {
+            return 4;
+        }
+        else
+        {
+            return 5;
+        }
+    }
+}
