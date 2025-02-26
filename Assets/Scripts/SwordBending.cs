3e5b716ca3a55e59ea4906ee67e1ca87d8f48ca1
BUDSTER16 <pwnagemm@hotmail.ca>
2025-02-26 12:12:12 -0500

    object can be grabbed and bent!

---
+++ Assets/Scripts/SwordBending.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.EventSystems;
+
+public class SwordBending : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private SpriteShapeController blade;
+    private Spline blade_spline;
+
+    private bool grabbing = false;
+    private int grab_point;
+
+
+    void Start()
+    {
+        blade = GetComponent<SpriteShapeController>();
+        blade_spline = blade.spline;
+    }
+
+    
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //if (Input.GetMouseButtonDown(0) && mousePos.y > blade.spline.GetPosition(2).y)
+        //{
+        //    blade_spline.SetPosition(2, new Vector3(blade.spline.GetPosition(2).x, blade.spline.GetPosition(2).y - 0.5f));
+        //    Debug.Log(mousePos.y);
+        //}
+        //else if(Input.GetMouseButtonDown(0) && mousePos.y < blade.spline.GetPosition(2).y)
+        //{
+        //    blade_spline.SetPosition(2, new Vector3(blade.spline.GetPosition(2).x, blade.spline.GetPosition(2).y + 0.5f));
+        //}
+
+        if (grabbing)
+        {
+            blade_spline.SetPosition(grab_point, mousePos);
+        }
+    }
+
+    void GrabPoint(Vector3 mouse_position)
+    {
+        Debug.Log("Grabbing!");
+
+        int closest_point = 0;
+        float dist_to_mouse = 0;
+
+        for (int i = 0; i < blade_spline.GetPointCount(); i++)
+        {
+            float point_to_mouse = MathF.Sqrt(MathF.Pow((mouse_position.x - blade_spline.GetPosition(i).x), 2) + MathF.Pow((mouse_position.y - blade_spline.GetPosition(i).y), 2));
+            if (point_to_mouse < dist_to_mouse || i == 0) 
+            {
+                dist_to_mouse = point_to_mouse;
+                closest_point = i;
+            }
+        }
+
+        grab_point = closest_point;
+        grabbing = true;
+    }
+
+    void EndGrab()
+    {
+        grabbing = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GrabPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (grabbing)
+        {
+            grabbing = false;
+        }
+    }
+}
